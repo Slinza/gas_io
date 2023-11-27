6f33628005295140a2a02d91bcd6f2b965125fa2
Damiano Dallapiccola <damiano.dallapiccola@gmail.com>
2023-11-27 22:03:34 +0100

    changed main screen

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
 
   @override
   Widget build(BuildContext context) {
